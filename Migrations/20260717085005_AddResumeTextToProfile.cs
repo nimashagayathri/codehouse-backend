3c4d113f7e918eeb2a61d195436d00263c249d73
nimashagayathri <nimashagayathri24@gmail.com>
2026-07-20 12:25:52 +0530

    Create 20260717085005_AddResumeTextToProfile.cs

---
+++ Migrations/20260717085005_AddResumeTextToProfile.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecruitmentPlatform.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddResumeTextToProfile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ResumeText",
+                table: "CandidateProfiles",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ResumeText",
+                table: "CandidateProfiles");
+        }
+    }
+}
